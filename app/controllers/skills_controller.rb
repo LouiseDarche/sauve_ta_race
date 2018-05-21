3ec0582c9f701f059dab9ad56bfb3601f031e4bb
Elsa Szymczak <elsaszymczak@gmail.com>
2018-05-21 17:55:18 +0200

    created the action for the skill controller

---
+++ app/controllers/skills_controller.rb
@@ -0,0 +1,24 @@
+class SkillsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+end
