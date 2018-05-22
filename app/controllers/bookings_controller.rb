86c29f5e941020c1ed8dd5355dbc0a4a245bf37f
Louise Darche <louise.darche@gmail.com>
2018-05-22 10:31:04 +0200

    added the right methods to the booking controller - were in controller test

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,9 @@
 class BookingsController < ApplicationController
+  def new
+    @skill = Skill.find(params[:skill_id])
+    @booking = Booking.new
+  end
+
+  def create
+  end
 end
--- test/controllers/bookings_controller_test.rb
+++ test/controllers/bookings_controller_test.rb
@@ -4,14 +4,5 @@ class BookingsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
-  def new
-    @skill = Skill.find(params[:skill_id])
-    @booking = Booking.new
-  end
-
-  def create
-
-
-  end
 
 end
