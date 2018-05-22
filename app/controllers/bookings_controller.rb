3c8d333c5242d64d3e1848b10cfd71b4f6f829d7
Louise Darche <louise.darche@gmail.com>
2018-05-22 12:26:10 +0200

    created booking policy

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,7 @@
 class BookingsController < ApplicationController
-  def new
-    @skill = Skill.find(params[:skill_id])
-    @booking = Booking.new
+  def index
+    @bookings = Booking.all
   end
 
-  def create
-  end
+
 end
---
+++ app/policies/booking_policy.rb
@@ -0,0 +1,9 @@
+class BookingPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope
+    end
+  end
+
+
+end
