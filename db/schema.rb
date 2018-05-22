07cbeeef04a83e62407dd9910821f05ade6d929e
Louise Darche <louise.darche@gmail.com>
2018-05-22 17:23:43 +0200

    updated with Marco and Elsa

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_21_143141) do
+ActiveRecord::Schema.define(version: 2018_05_22_150616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2018_05_21_143141) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
     t.index ["user_id"], name: "index_skills_on_user_id"
   end
 
@@ -55,6 +56,7 @@ ActiveRecord::Schema.define(version: 2018_05_21_143141) do
     t.string "location"
     t.string "status"
     t.string "specie"
+    t.string "image_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
