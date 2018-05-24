081d7d49b8429a9bf76717c215b7d96cd4f48502
Louise Darche <louise.darche@gmail.com>
2018-05-24 12:25:43 +0200

    button on form to upload a file for the skill is ok

---
+++ app/javascript/packs/uploadbtn.js
@@ -0,0 +1,8 @@
+uploadBtn = document.getElementById('upload-btn');
+
+function updateText() {
+  console.log(this.firstChild);
+  this.firstChild.textContent = 'File well uploaded';
+}
+
+uploadBtn.addEventListener('change', updateText);
